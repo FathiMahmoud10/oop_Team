e57cad11229d9511325edbf51282ec0d2db543ae
Fathhi <Afaq@DESKTOP-65EEVFH>
2026-01-20 17:50:30 +0200

    load Data and Student Analyze by degree

--- OOP_Pro/Models/Student.cs
+++ OOP_Pro/Models/Student.cs
@@ -4,7 +4,12 @@ using System.Text;
 
 namespace OOP_Pro.Models
 {
-    internal class Student
+    public class Student
     {
+        public int Id { get; set; } // كود الطالب 
+        public string? Name { get; set; } // اسم الطالب 
+        public string? Course { get; set; } // اسم الكورس 
+        public int Grade { get; set; } // الدرجة 
+        public int Attendance { get; set; } // درجة لحضور 
     }
 }
--- OOP_Pro/Program.cs
+++ OOP_Pro/Program.cs
@@ -1,10 +1,44 @@
-﻿namespace OOP_Pro
+﻿using OOP_Pro.Classes;
+using OOP_Pro.Models;
+
+namespace OOP_Pro
 {
     internal class Program
     {
+        
+        
+        /* Added by Fathi 
+             
+             بيدخل اللي اسمة TextReading 
+            و بيرجع بالبيانات الموجوده في الملف 
+            و بيخزنها في Arry 
+
+             */
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Student[] students = TextReading.ReadStudentsFromFile("students.txt");
+
+            Console.WriteLine($"All Students");
+
+
+
+            foreach (var s in students)
+            {
+                Console.WriteLine($"{s.Name} - {s.Course} - Grade: {s.Grade}");
+            }
+
+
+            Console.WriteLine($"\n");
+
+
+            if (students != null)
+            {
+                StudentOrgnize.StudentAnalyze(students);
+            }
+            else
+            {
+                Console.WriteLine("No students found.");
+            }
         }
     }
 }
